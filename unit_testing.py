6be2d8f026ca6a915f2ceda5389ed94ce9a5cac8
sameeyaa <sameeya005@gmail.com>
2025-10-16 14:20:10 +0100

    simple unit test

---
+++ unit_testing.py
@@ -0,0 +1,10 @@
+import unittest
+from week_2 import add
+
+class test(unittest.TestCase):
+    def test_add(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
+
+if __name__ == '__main__':
+    unittest.main()
