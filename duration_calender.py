b2db04af71cfbf0cd4dfca1453686299495ecf22
sameeyaa <sameeya005@gmail.com>
2025-10-21 21:43:02 +0100

    fixing errors

--- duration_calender.py
+++ duration_calender.py
@@ -10,7 +10,8 @@ print("Today's date:", today) #the date is formatted year-month-day (YYYY-MM-DD)
 
 #ask user to input the second date that they want to calculate the difference against
 input_str = input("Please enter the date you want to calculate from in the format (YYYY-MM-DD): ")
-date2 = datetime.strptime(input_str, "%Y-%m-%d").date()  #parse the string to format as a date
+date2 = datetime.strptime(input_str, "%Y-%m-%d").date()  
+#parse the string to format as a date
 
 #calculate the difference between the inputted date and today
 difference = (today - date2)
