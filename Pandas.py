08fec4b0d72c5998b3be9afc2305e06736db82eb
sameeyaa <sameeya005@gmail.com>
2025-10-23 14:04:34 +0100

    find out how many columns there are and which columns I need

--- Pandas.py
+++ Pandas.py
@@ -1,5 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  #will help create graphs
 
+#load the datasets
 df = pd.read_csv('/Users/sameeyaali/Downloads/US-2016-primary.csv',delimiter = ";")
 print(df)
+
+print(df.columns)  #names of all the columns
+#columns: state, state abbreviation, countyr, fips, party, candidate, votes, fraction votes
