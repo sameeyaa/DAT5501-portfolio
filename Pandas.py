79d4fdd5eaaf8c2dc604154acb3221a3aa3102f5
sameeyaa <sameeya005@gmail.com>
2025-10-28 21:14:46 +0000

    add file

--- Pandas.py
+++ Pandas.py
@@ -7,3 +7,19 @@ print(df)
 
 print(df.columns)  #names of all the columns
 #columns: state, state abbreviation, countyr, fips, party, candidate, votes, fraction votes
+
+#choose my candidate I want to analyse from the list
+chosen_candidate= "Carly Firiona"
+
+#filter the data to only select data to do with Carly Firiona
+candidate_df = df[df["candidate"]== chosen_candidate]
+
+#check if specific data is only presented
+print(candidate_df)
+
+#plot the histogram listing the amount of votes from each state
+plt.hist(candidate_df["votes"], bins = 15)
+plt.xlabel(f"Number of Votes")
+plt.ylabel("Number of States")
+plt.title(f"Number of Votes for Carly Firiona by each State (2016)")
+plt.show()
