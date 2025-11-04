a62f28f48cf341bb47647322acbe3c6c68c140aa
sameeyaa <sameeya005@gmail.com>
2025-11-04 20:18:50 +0000

    fix graph so the visuals show

--- Pandas.py
+++ Pandas.py
@@ -1,25 +1,26 @@
 import pandas as pd
-import matplotlib.pyplot as plt  #will help create graphs
+import matplotlib.pyplot as plt  # will help create graphs
 
-#load the datasets
-df = pd.read_csv('/Users/sameeyaali/Downloads/US-2016-primary.csv',delimiter = ";")
-print(df)
+#load the dataset
+df = pd.read_csv('/Users/sameeyaali/Downloads/US-2016-primary.csv', delimiter=";")
 
-print(df.columns)  #names of all the columns
-#columns: state, state abbreviation, countyr, fips, party, candidate, votes, fraction votes
+print(df.columns)  # names of all the columns
 
-#choose my candidate I want to analyse from the list
-chosen_candidate= "Carly Firiona"
+#chosen candidate
+chosen_candidate = "Carly Fiorina"
 
-#filter the data to only select data to do with Carly Firiona
-candidate_df = df[df["candidate"]== chosen_candidate]
-#check if specific data is only presented
+#filter the data for that candidate
+candidate_df = df[df["candidate"] == chosen_candidate]
 print(candidate_df)
 
-#plot the histogram listing the amount of votes from each state
-plt.hist(candidate_df["votes"], bins = 15)
-plt.xlabel(f"Number of Votes")
+#change votes to a number format
+candidate_df["votes"] = pd.to_numeric(candidate_df["votes"], errors="coerce")
+candidate_df = candidate_df.dropna(subset=["votes"])
+
+#plot the histogram
+plt.hist(candidate_df["votes"], bins=15)
+plt.xlabel("Number of Votes")
 plt.ylabel("Number of States")
-plt.title(f"Number of Votes for Carly Firiona by each State (2016)")
+plt.title(f"Number of Votes for {chosen_candidate} by each State (2016)")
 plt.show()
 
