12cb6d953c34081d50eb05a8572da0b3b4ea142b
sameeyaa <sameeya005@gmail.com>
2025-11-10 11:33:28 +0000

    starting code

---
+++ sorting_price.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+df = pd.read_csv('/Users/sameeyaali/Downloads/NVIDIA CORPORATION (08-05-2024 _ 11-07-2025).csv')
+print(df)
+
+df['Close'] = df['Close'].astype(str).str.replace(',' , '').astype(float)
+
+prices = df['Close'].values
+change_in_p = np.diff(prices)
+
+n_values = np.arrange(7, len(change_in_p) + 1)
+T = []
+
+for n in n_values:
+    sample = change_in_p[:n]
+    start = time.perf_counter()
+    sorted(sample)
+    end = time.perf_counter()
+    T.append (end - start)
+
+    nlogn = n_values * np.log(n_values)
+    nlogn_scaled = nlog / nlogn.max * max(T)
+
+    
