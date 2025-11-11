70de5cdc4e2a67a021350e026bededb92541d94d
sameeyaa <sameeya005@gmail.com>
2025-11-11 22:41:23 +0000

    enabling player to enter an input value and determining outcome

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,5 +3,14 @@ import random
 options =  ["rock" , "paper", "scissors"]
 
 def get_game_choice():
-    " Pick rock,paper or scsissors and see if the computer will beat you!"
+#randomise which option the computer chooses
     return random.choice(options)
+
+#Dictate the winner of a match
+def get_winner(player_option, coomputer_option):
+    player_option = player_option.lower()
+    coomputer_option = compile.lower()
+
+    if player_option not in options or coomputer_option not in options:
+        raise ValueError("Invalid input! Please pick 'rock', 'paper' or 'scissors")
+    
