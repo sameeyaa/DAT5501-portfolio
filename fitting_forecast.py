9c000c02a7815fca155fa8e7e96d1d36bd22d7a5
sameeyaa <sameeya005@gmail.com>
2025-11-17 12:30:50 +0000

    refining date range

--- fitting_forecast.py
+++ fitting_forecast.py
@@ -7,20 +7,20 @@ import numpy as np
 
 #convert my chosen csv into a dataframe
 life_expectancy_df = pd.read_csv("life-expectancy.csv")
-#print(life_expectancy_df.head())
+print(life_expectancy_df.head())
 
 #getting rid of any columns that are not needed - cleaning data
 life_expectancy_df = life_expectancy_df.drop(columns = ['Entity'])
 
-#print(life_expectancy_df.columns.tolist())
-#print(life_expectancy_df.head())
+print(life_expectancy_df.columns.tolist())
+print(life_expectancy_df.head())
 
 #change column name to a simpler name
 life_expectancy_df.rename(columns = {'Period life expectancy at birth' : 'Life expectancy at birth'}, inplace = True)
-#print(life_expectancy_df.head())
+print(life_expectancy_df.head())
 
 #masking the data for a specific timeframe
-mask1 = life_expectancy_df['Year'] >= 1923
+mask1 = life_expectancy_df['Year'] >= 1940
 mask2 = life_expectancy_df['Year'] <= 2023
 
 first_range = life_expectancy_df[mask1]
@@ -29,8 +29,7 @@ range_life_expectancy_df = first_range[mask2]
 
 #set the variables for the polynomial graph
 # x axis = Year    y axis = Life expectancy at birth
-
-
+#mask and choose one entity 'Cayman Islands'
 mask3 = range_life_expectancy_df['Code'] == 'CYM'
 cym_life_expectancy = range_life_expectancy_df[mask3]
 print(cym_life_expectancy.head())
@@ -39,7 +38,7 @@ years = cym_life_expectancy['Year']
 #years_centered = years - years.mean()
 life_expectancy = range_life_expectancy_df['Life expectancy at birth']
 
-xp = np.linspace(1923, 2023 + 10, 300)
+xp = np.linspace(1940, 2023 + 10, 300)
 degree = 10
 
 def poly():
