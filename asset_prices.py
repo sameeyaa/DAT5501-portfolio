3aa945cc87e9a8a245860124b94c26d6946fae9c
sameeyaa <sameeya005@gmail.com>
2025-11-17 20:21:25 +0000

    datetime format

--- asset_prices.py
+++ asset_prices.py
@@ -5,7 +5,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import datetime
 
 #create a dataframe from the Tesla Inc csv file
 df = pd.read_csv('TESLA INC (08-27-2024 _ 11-17-2025).csv')
 print(df.head())
+
+#ensure dates are in datetime format
+df['Date'] = pd.to_datetime(df['Date'])
+print(df.head())
+
