656e6a49f6075a471955b162aa33ad61dbef879f
sameeyaa <sameeya005@gmail.com>
2025-11-17 11:34:14 +0000

    cleaning the data

--- fitting_forecast.py
+++ fitting_forecast.py
@@ -8,10 +8,14 @@ import scipy
 life_expectancy_df = pd.read_csv("life-expectancy.csv")
 print(life_expectancy_df.head())
 
-#getting rid of any columns that are not needed
+#getting rid of any columns that are not needed - cleaning data
 life_expectancy_df = life_expectancy_df.drop(columns = ['Entity' , 'Code' ])
 
 print(life_expectancy_df.columns.tolist())
 print(life_expectancy_df.head())
 
 #change column name to a simpler name
+life_expectancy_df.rename(columns = {'Period life expectancy at birth' : 'Life expectancy at birth'}, inplace = True)
+print(life_expectancy_df.head())
+
+#masking the data for a specific
