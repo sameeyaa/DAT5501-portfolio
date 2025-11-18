b7acd67ffbc33524c4a48b8fec26d1f961487f6b
sameeyaa <sameeya005@gmail.com>
2025-11-18 17:26:24 +0000

    feeding interaction

--- digital_pet.py
+++ digital_pet.py
@@ -16,4 +16,19 @@ class DigitalPet:
         self.sleeping = False
         self.interactions = []
 
-        
+    #feeding interaction
+    def feed(self):
+        #when the pet is fed, hunger vital will increase by 3, happiness will also increase by 2
+        if self.hunger < 10:
+            self.hunger += 3
+            self.happiness += 2
+            self.interactions.append("fed")
+            print(f"You fed {self.name}.")
+            self.speak("Yummy! That was tasty, thank you!")
+            #when hunger level is 10, pet will mention it is full and cannot be fed
+            if self.hunger == 10:
+                print(f"{self.name} is full and does not want to eat anymore.")
+            else:
+                print(f"{self.name} is already full and cannot eat anymore!")
+
+
