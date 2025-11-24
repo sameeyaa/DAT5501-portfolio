b85069f10cac5790a4be5291e53ec038d8813c28
sameeyaa <sameeya005@gmail.com>
2025-11-24 12:41:06 +0000

    importing data csv files

---
+++ machine_learning.py
@@ -0,0 +1,7 @@
+#decision tree mini project
+#identify a dataset on the UC Irvine Machine Learning Repository
+#use a decision tree to classify and predict
+#evaluate your model with precision and recall
+#use scikit-learn
+
+#my chosen data topic is Car Evaluations
