2e761e97311a92a1ff644bcbb4ecb7e6aeccea18
sameeyaa <sameeya005@gmail.com>
2025-11-24 18:22:19 +0000

    debugging

--- machine_learning.py
+++ machine_learning.py
@@ -5,3 +5,14 @@
 #use scikit-learn
 
 #my chosen data topic is Car Evaluations
+
+#merge the 3 csv files to make it easier to clean
+import pandas as pd
+
+df1 = pd.read_csv('car.c45-names.csv')
+df2 = pd.read_csv('car.csv')
+df3 = pd.read_csv('car.names.csv')
+
+merged_df = pd.concat([df1, df2, df3], ignore_index=True)
+merged_df.to_csv("merged.csv", index = False)
+print("Merged csv saved as merged.csv.")
