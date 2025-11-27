3cb68c6dfed67248ffcdabf9a5752c41353f3465
sameeyaa <sameeya005@gmail.com>
2025-11-27 13:40:08 +0000

    import necessary libraries

--- machine_learning.py
+++ machine_learning.py
@@ -1,4 +1,8 @@
 from ucimlrepo import fetch_ucirepo 
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import pandas as pd
   
 # fetch dataset 
 iris = fetch_ucirepo(id=53) 
@@ -12,3 +16,6 @@ print(iris.metadata)
   
 # variable information 
 print(iris.variables) 
+
+#set the target sppecies we are looking at
+le = LabelEncoder()
