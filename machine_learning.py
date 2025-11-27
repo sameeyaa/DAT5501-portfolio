c3b18d55753dde3722125d17dd5d9d4510cfcde7
sameeyaa <sameeya005@gmail.com>
2025-11-27 13:37:37 +0000

    changing dataset

--- machine_learning.py
+++ machine_learning.py
@@ -1,34 +1,14 @@
-
 from ucimlrepo import fetch_ucirepo 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-import matplotlib.pyplot as plt
-import pandas as pd
-
+  
 # fetch dataset 
-car_evaluation = fetch_ucirepo(id=19) 
+iris = fetch_ucirepo(id=53) 
   
 # data (as pandas dataframes) 
-X = car_evaluation.data.features 
-y = car_evaluation.data.targets 
+X = iris.data.features 
+y = iris.data.targets 
   
 # metadata 
-#print(car_evaluation.metadata) 
+print(iris.metadata) 
   
 # variable information 
-#print(car_evaluation.variables) 
-
-#create categorical columns
-label_encoders = {}
-X_encoded = X.copy()
-
-for col in X.columns:
-    le = LabelEncoder
-    X_encoded[col] = le.fit_transform(X[col])
-    label_encoders[col] = le
-
-y_encoded = LabelEncoder().fit_transform(y.iloc[:, 0])
-
-
-
-
+print(iris.variables) 
