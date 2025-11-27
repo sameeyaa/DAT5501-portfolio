61d4e16981cd79b2c20852a9fdc95877adea52d5
sameeyaa <sameeya005@gmail.com>
2025-11-27 11:33:24 +0000

    importing necessary libraries

--- machine_learning.py
+++ machine_learning.py
@@ -1,6 +1,10 @@
 
 from ucimlrepo import fetch_ucirepo 
-  
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
+import pandas as pd
+
 # fetch dataset 
 car_evaluation = fetch_ucirepo(id=19) 
   
